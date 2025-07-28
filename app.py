34cf716d277d6a0554b491cd4b5993b5d2d55e4a
Henrique <henrique.218@gmail.com>
2025-07-28 00:19:57 -0300

    app funcionando

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from workshop-docker!")
+import streamlit as st
 
+def hello_world():
+    return "Hello World teste 2!"
+
+def main():
+    st.write(hello_world())
 
 if __name__ == "__main__":
     main()
